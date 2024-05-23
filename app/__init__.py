a1f3530736bbb0cb237e2fc314a3c380a087297a
GasinskiN <gasinskinorman@gmail.com>
2024-05-23 18:56:33 +0200

    file deletions and cache

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 db = SQLAlchemy()
     
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__,'/static')
 
 
     #inicjacja bazy danych
--- app/main.py
+++ app/main.py
@@ -10,9 +10,38 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('homepage.html')
 
 @main.route('/profile')
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.username)
+
+
+@main.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@main.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@main.route('/leaderboard')
+def leaderboard():
+    return render_template('leaderboard.html')
+
+
+@main.route('/gameplay')
+def gameplay():
+    return render_template('gameplay.html')
+
+
+@main.route('/forum')
+def forum():
+    return render_template('forum.html')
+
+@main.errorhandler(404)
+def page_not_found(e):
+    return render_template("error.html")
