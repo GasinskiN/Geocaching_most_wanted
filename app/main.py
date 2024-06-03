800b0bea7708f29b202481d194676be8b5fcbd94
Asderq57 <korlik2002@gmail.com>
2024-06-03 18:39:03 +0200

    profile api added

--- app/main.py
+++ app/main.py
@@ -36,7 +36,7 @@ def home():
 #     # return render_template('profile.html', name=current_user.username, profile=profile)
 #     return render_template("profile.html",name=current_user.username, profile = profile, bridges = bridges, points = 8000)
 
-@main.route('/profile')
+@main.route('/profile', methods=['GET'])
 @login_required
 @swag_from({
     'responses': {
@@ -51,70 +51,19 @@ def home():
     }
 })
 def profile():
-    # Fetch the current user from the database
-    user = User.query.get(current_user.get_id())
-    
-    # Get the count of visited bridges
-    visited_bridges_count = len(user.visited_bridges)
-    
-    # Fetch the last 3 visited bridges
-    last_three_bridges = user.visited_bridges[-3:]
-    
-    profile_img = "../static/public/Profile_img.jpg"
+    return render_template("profile.html")
 
-    achievements = [("Konto że hej", "../static/public/koty_za_ploty.png")]
-
-
-    if visited_bridges_count > 1:
-        achievements.append(("Pierwsze koty za płoty", "../static/public/koty_za_ploty.png"))
-    
-    if visited_bridges_count > 3:
-        achievements.append(("Teraz już tylko z górki", "../static/public/koty_za_ploty.png"))
-
-    if user.points > 9000:
-        achievements.append(("Jest większe niż 9000", "../static/public/koty_za_ploty.png"))
-
-
-    return render_template("profile.html", 
-                           name=current_user.user_id, 
-                           profile=profile_img, 
-                           bridges=last_three_bridges, 
-                           points=visited_bridges_count,
-                           achievements=achievements
-                           )
-
-@main.route('/login')
-@swag_from({
-    'responses': {
-        200: {
-            'description': 'Login page',
-            'content': {
-                'text/html': {
-                    'example': '<html>Login Page</html>'
-                }
-            }
-        }
+@main.route('/api/profile/user', methods=['POST'])
+@login_required
+def get_uesr_profile():
+    user = User.query.filter_by(user_id=current_user.user_id).first()
+    payload = {
+        'username': user.username,
+        'points': user.points,
+        'visited_bridges': user.visited_bridges,
+        'achievements': user.achevements
     }
-})
-def login():
-    return render_template('login.html')
-
-
-@main.route('/register')
-@swag_from({
-    'responses': {
-        200: {
-            'description': 'Register page',
-            'content': {
-                'text/html': {
-                    'example': '<html>Register Page</html>'
-                }
-            }
-        }
-    }
-})
-def register():
-    return render_template('register.html')
+    return jsonify(payload), 200
 
 
 @main.route('/leaderboard')
@@ -148,43 +97,6 @@ def get_users_sorte_by_points():
         payload.append(data)
         rank += 1
     return jsonify(payload), 200
-
-
-
-@main.route('/api/gameplay')
-@login_required
-@swag_from({
-    'responses': {
-        200: {
-            'description': 'Gameplay page',
-            'content': {
-                'text/html': {
-                    'example': '<html>Gameplay Page</html>'
-                }
-            }
-        }
-    }
-})
-def gameplay():
-    bridges = Bridge.query.all()
-    return render_template('gameplay_page.html', bridges=bridges)
-
-# Powinno być post na baze danych ale w ramach testu czy działa daje na ten sam url post
-# @main.route('/forum/<bridgeid>', methods = ['POST', 'GET'])
-# @login_required
-# def forum(bridgeid):
-#     comments = [{'text':'Komentarz pierwszy', 'username':'Pioter32'},{'text':'Fajny most', 'username':'Janusz'}]
-#     image = "../static/public/"+ bridgeid +".jpg"
-#     if request.method == 'POST':
-
-#         # Tutaj klucz to jest text a value to jest to co jest napisane w komentarzu, trzeba by to połączyć z 
-#         # nazwą użytkownika i dodać do bazy danych jakby co to można zobaczyć w konsoli
-#         for key, value in request.form.items():
-#             print(key, value)
-
-#         return render_template('forum.html', comments = comments, image=image)
-#     if request.method == "GET":
-#         return render_template('forum.html', comments = comments, image=image)
 
 @main.route('/forum/<int:bridgeid>', methods=['GET'])
 @login_required
