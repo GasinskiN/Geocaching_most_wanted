e982c5572963f2f76e0fe91aaf484b4e9e6a58c5
SzymonLitwicki <litwickiszymon@gmail.com>
2024-06-03 23:03:28 +0200

    dodane uruchamianie z docker-compose i działa lokalizacja

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,28 @@ from flask_login import LoginManager
 from flasgger import Swagger
 from werkzeug.security import generate_password_hash
 
+
 db = SQLAlchemy()
+
+
+def create_admin_user():
+    from .models import User
+    admin_username = "admin@mostwanted.com"
+    admin_password = "admin"
+
+    existing_admin = User.query.filter_by(username=admin_username).first()
+    if existing_admin:
+        return
+
+    admin_user = User(
+        username=admin_username,
+        password=generate_password_hash(admin_password, method='pbkdf2:sha256'),
+        role='admin'
+    )
+    db.session.add(admin_user)
+    db.session.commit()
+
+
 
 def create_app():
     app = Flask(__name__,'/static')
@@ -52,20 +73,4 @@ def create_app():
     
     return app
 
-
-def create_admin_user():
-    from .models import User
-    admin_username = "admin@mostwanted.com"
-    admin_password = "admin"
-
-    existing_admin = User.query.filter_by(username=admin_username).first()
-    if existing_admin:
-        return
-
-    admin_user = User(
-        username=admin_username,
-        password=generate_password_hash(admin_password, method='pbkdf2:sha256'),
-        role='admin'
-    )
-    db.session.add(admin_user)
-    db.session.commit()
+app = create_app()
